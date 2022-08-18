832fc1194cf9848ad22bcf0061d7815e1c9e3f87
skekehdwn <krtdj@naver.com>
2022-08-18 11:08:43 +0900

    base_views.py   - menu.py import 내용 추가   - menuListDB menuSetting() 호출방식으로 변경 menu.py 생성   - menuSetting DB연결 및 menusetting값 호출 함수 생성 setting.json   - MenuSettingTNM을 추가

---
+++ common/menu.py
@@ -0,0 +1,55 @@
+import psycopg2
+import json
+import pandas as pd
+from datetime import datetime, timedelta
+
+with open("setting.json", encoding="UTF-8") as f:
+    SETTING = json.loads(f.read())
+DataLoadingType = SETTING['MODULE']['DataLoadingType']
+DBHost = SETTING['DB']['DBHost']
+DBName = SETTING['DB']['DBName']
+DBUser = SETTING['DB']['DBUser']
+DBPwd = SETTING['DB']['DBPwd']
+MenuTNM = SETTING['DB']['MenuTNM']
+
+
+def MenuSetting():
+    try:
+        MSDL = []
+        Conn = psycopg2.connect('host={0} dbname={1} user={2} password={3}'.format(DBHost, DBName, DBUser, DBPwd))
+        Cur = Conn.cursor()
+
+        query = """
+            select 
+                *
+            from
+                """ + MenuTNM + """
+
+            """
+
+        Cur.execute(query)
+        RS = Cur.fetchall()
+        # for R in RS:
+        #     MSDL.append(R)
+        # return MSDL
+
+        #print(RS)
+        DFL=[]
+        for d in RS:
+            ID = d[0]
+            MenuID = d[1]
+            MenuName = d[2]
+            MenuUrl = d[3]
+            MenuUse = d[4]
+            MenuNote = d[5]
+            MenuCD = d[6]
+            MenuImg = d[7]
+
+            DFL.append([ID, MenuID, MenuName, MenuUrl, MenuUse, MenuNote, MenuCD, MenuImg])
+            DFC = ['id', 'menuID', 'menuName','menuUrl', 'menuUse', 'menuNote', 'menuCD', 'menuImg']
+        DF = pd.DataFrame(DFL, columns=DFC).sort_values(by="id", ascending=False).reset_index(drop=True)
+        DC=DF.to_dict('records')
+        return DC
+    except:
+        print(MenuTNM+' Menu Table connection(Select) Failure')
+
