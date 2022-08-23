56475dfd0f5e8a79dee581bfbbe147c2f4091f98
skekehdwn <krtdj@naver.com>
2022-08-23 10:05:31 +0900

    login.html  -url 변경 (login, signup)  -error 알람화면 추가  -주석 삭제 signup.html  -url 변경 (signup)  -error 알람화면 추가  -가입실패시 value값 저장 nav.js  -로그아웃경로 수정 navbar.html  -sessionid, sessionmail 추가 setting.json  -UserTNM 추가 settings.py  -로그인시 이동페이지 주석처리 common/urls.py  -미사용으로 인한 전체 주석처리 config/urls.py  -urls 뷰페이지 연결추가 및 변경 common/views.py  -DB연결부분추가  -DB호출방식 signup 함수 변경  -DB호출방식 login 함수 추가  -DB호출방식 logout 함수 추가  -DB호출방식 Selectuser, createuser 함수추가

--- common/views.py
+++ common/views.py
@@ -1,18 +1,141 @@
-from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from common.forms import UserForm
+import hashlib
+import psycopg2
+import json
+
+with open("setting.json", encoding="UTF-8") as f:
+    SETTING = json.loads(f.read())
+DataLoadingType = SETTING['MODULE']['DataLoadingType']
+DBHost = SETTING['DB']['DBHost']
+DBName = SETTING['DB']['DBName']
+DBUser = SETTING['DB']['DBUser']
+DBPwd = SETTING['DB']['DBPwd']
+UserTNM = SETTING['DB']['UserTNM']
+
 
 
 def signup(request):
-    if request.method == "POST":
-        form = UserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)  # 사용자 인증
-            login(request, user)  # 로그인
-            return redirect('../login')
-    else:
-        form = UserForm()
-    return render(request, 'common/signup.html', {'form': form})
+    if request.method == "GET":
+        return render(request, 'common/signup.html')
+
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        re_password = request.POST.get('re_password')
+        email = request.POST.get('email')
+        customCheck1 = request.POST.get('customCheck1')
+        res_data = {}
+        if not (username and password and re_password and email):
+            res_data['username'] = username
+            res_data['email'] = email
+            res_data['error'] = "모든 값을 입력해야 합니다."
+            return render(request, 'common/signup.html', res_data)
+        if password != re_password:
+            res_data['username'] = username
+            res_data['email'] = email
+            res_data['error'] = '비밀번호가 다릅니다.'
+            return render(request, 'common/signup.html', res_data)
+        if customCheck1  != '1' :
+            res_data['username'] = username
+            res_data['email'] = email
+            res_data['error'] = '동의를 눌러주세요.'
+            return render(request, 'common/signup.html', res_data)
+        else:
+            RS = createUsers(username,password,email)
+            if RS == "1" :
+                res_data['error'] = "회원가입에 성공하였습니다."
+                return render(request, 'common/login.html', res_data)
+            else :
+                res_data['error'] = "아이디가 존재합니다."
+                res_data['email'] = email
+                return render(request, 'common/signup.html', res_data)
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'common/login.html')
+
+    # POST 방식 요청 -> 사용자가 보내는 데이터와 데이터베이스의 정보 일치여부 확인
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        # 응답 데이터
+        res_data = {}
+
+        # 모든 필드를 채우지 않았을 경우
+        if not (username and password):
+            res_data['error'] = '아이디 또는 비밀번호를 입력해 주세요.'
+            return render(request, 'common/login.html', res_data)
+        # 모든 필드를 채웠을 경우
+        else:
+            RS = selectUsers(username, password)
+            if RS == None:
+                res_data['error'] = '아이디 또는 비밀번호가 일치하지 않습니다'
+                return render(request, 'common/login.html', res_data)
+
+            else:
+                request.session['sessionid']=RS[4]
+                request.session['sessionemail']=RS[7]
+                return redirect('../dashboard')
+
+def logout(request):
+    if request.session['sessionid'] :
+        del(request.session['sessionid'])
+        del(request.session['sessionemail'])
+
+    return render(request, 'common/login.html')
+
+
+def selectUsers(username, password):
+    try:
+        hashpassword = hashlib.sha256(password.encode()).hexdigest()
+        #print(hashpassword)
+
+        Conn = psycopg2.connect('host={0} dbname={1} user={2} password={3}'.format(DBHost, DBName, DBUser, DBPwd))
+        Cur = Conn.cursor()
+
+        query = """
+            select 
+                *
+            from
+                """ + UserTNM + """
+            where
+                username = '""" + username + """'
+            and
+                password = '""" + hashpassword + """'   
+
+            """
+
+        Cur.execute(query)
+        RS = Cur.fetchone()
+        #print(RS)
+        return RS
+    except:
+        print(UserTNM + ' Table connection(Select) Failure')
+
+
+def createUsers(username, password, email):
+    try:
+        hashpassword = hashlib.sha256(password.encode()).hexdigest()
+        Conn = psycopg2.connect('host={0} dbname={1} user={2} password={3}'.format(DBHost, DBName, DBUser, DBPwd))
+        Cur = Conn.cursor()
+        query =""" 
+        INSERT INTO 
+            auth_user 
+            (id, username, password, email, date_joined) 
+        VALUES  
+            (nextval('auth_user_id_seq'),
+                '""" + username + """',
+                '""" + hashpassword + """' ,
+                '""" + email + """' ,
+                now());
+        """
+        Cur.execute(query)
+        Conn.commit()
+        Conn.close()
+        a = "1"
+        return a
+    except:
+        print(UserTNM + ' Table connection(Select) Failure')
+        a = "0"
+        return a
--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,19 @@
-from django.contrib import admin
-from django.urls import path, include
 
+from django.urls import path
 from web.views import base_views
+from common import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('web/', include('web.urls')),
-    path('common/', include('common.urls')),
-    path('', base_views.index, name='index'),  # '/' 에 해당되는 path
+    path('', views.login, name=''),
+    path('login/', views.login, name='login'),
     path('dashboard/', base_views.dashboard, name='dashboard'),
-    path('question/list/', base_views.list, name='list'),
+    path('signup/', views.signup, name='signup'),
+    path('dashboard/logout/', views.logout, name='logout'),
+
+    # path('admin/', admin.site.urls),
+    # path('web/', include('web.urls')),
+    # path('common/', include('common.urls')),
+    # #path('', base_views.index, name='index'),  # '/' 에 해당되는 path
+    # path('question/list/', base_views.list, name='list'),
+
 ]
