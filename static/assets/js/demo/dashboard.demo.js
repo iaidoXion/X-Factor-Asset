079f22bfce4f6a671517e0b1a281b18c0d926c0d
mendoda20 <mendoda20@gmail.com>
2022-09-08 09:44:24 +0900

    1. base.html - script import 위치 변경
    2. dashboard.html
    - 미니 차트 리스트 name 및 value 너비 조정
    - 맵차트 수정
    
    3. dashboard.css
    - olddashboard style 주석 처리
    
    4. dashboard.js
    - 맵차트 버튼 기능 구현
    
    5. dashboard.demo.js
    - 차트 스타일 수정
    - 맵 차트 수정
    
    6. app.min.css
    - apexdountchart tooltip style 수정

--- static/assets/js/demo/dashboard.demo.js
+++ static/assets/js/demo/dashboard.demo.js
@@ -79,7 +79,7 @@ var handleRenderChart = function () {
       labels: {
         style: {
           colors: "#fff",
-          fontSize: "3px",
+          fontSize: "8px",
           fontFamily: app.font.family,
           fontWeight: 400,
           cssClass: "apexcharts-xaxis-label",
@@ -346,7 +346,7 @@ var handleRenderChart = function () {
 
 var apexColumnChartOptions = {
 		chart: {
-			height: 100,
+			height: 120,
 			type: 'bar',
 			toolbar: {
 				show: false
@@ -378,7 +378,11 @@ var apexColumnChartOptions = {
 			categories: barName,
 			labels: {
 				show: true,
-                style: {}
+                style: {
+					colors: '#fff',
+					fontSize: '9px',
+					cssClass: 'apexcharts-xaxis-label',
+				}
 			}
 		},
 		yaxis: {
@@ -392,7 +396,7 @@ var apexColumnChartOptions = {
 		tooltip: {
 			theme: 'dark',
 			x: {
-				show: false
+				show: true
 			},
 			y: {
 				title: {
@@ -518,8 +522,8 @@ var apexLineChartOptions = {
 			},
 			y: {
 				title: {
-					formatter: function (seriesName) {
-						return ''
+					formatter: function (val) {
+						return '' + val
 					}
 				},
 				formatter: (value) => { return '' + value },
@@ -560,7 +564,7 @@ var apexPieChartOptions = {
 		plotOptions: {
 			pie: {
 				dataLabels: {
-					offset: 2
+					offset: -10
 				}
 			}
 		},
@@ -599,8 +603,8 @@ var apexPieChartOptions = {
 			},
 			y: {
 				title: {
-					formatter: function (seriesName) {
-						return ''
+					formatter: function (val) {
+						return '' + val
 					}
 				},
 				formatter: (value) => { return '' + value },
@@ -640,7 +644,7 @@ var apexDountChartOptions = {
 			enabled: true,
 			formatter(val, opts) {
 				const name = opts.w.globals.labels[opts.seriesIndex]
-				return [name, val.toFixed(1) + '%']
+				return [val.toFixed(1) + '%']
 			},
 			style: {
 				fontSize: '10px',
@@ -667,12 +671,12 @@ var apexDountChartOptions = {
 		tooltip: {
 			theme: 'dark',
 			x: {
-				show: false
+				show: true
 			},
 			y: {
 				title: {
-					formatter: function (seriesName) {
-						return ''
+					formatter: function (val) {
+						return '' + val
 					}
 				},
 				formatter: (value) => { return '' + value },
@@ -692,7 +696,7 @@ var apexDountChartOptions = {
 	group = [];
 	var radar_list = [];
 	var category_list = [];
-	console.log(a);
+//	console.log(a);
 	
 	for (var i = 0; i < 5; i++) {
     	group.push(a.AssociationDataList.nodeDataList[i].group);
@@ -715,7 +719,7 @@ var apexDountChartOptions = {
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'RAM Usage Exceeded' ||
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'CPU Consumption is Excess' ||
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'Running Process is Exceeded') {
-						console.log(a.AssociationDataList.nodeDataList[j].alarmCount);
+//						console.log(a.AssociationDataList.nodeDataList[j].alarmCount);
 					} else {
 						list[0] = parseInt(0);
 						alarm_list[0] = a.AssociationDataList.nodeDataList[j].alarmCase;
@@ -731,7 +735,7 @@ var apexDountChartOptions = {
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'RAM Usage Exceeded' ||
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'CPU Consumption is Excess' ||
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'Running Process is Exceeded') {
-						console.log(a.AssociationDataList.nodeDataList[j].alarmCount);
+//						console.log(a.AssociationDataList.nodeDataList[j].alarmCount);
 					} else {
 						list[1] = parseInt(0);
 						alarm_list[1] = a.AssociationDataList.nodeDataList[j].alarmCase;
@@ -747,7 +751,7 @@ var apexDountChartOptions = {
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'RAM Usage Exceeded' ||
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'CPU Consumption is Excess' ||
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'Running Process is Exceeded') {
-						console.log(a.AssociationDataList.nodeDataList[j].alarmCount);
+//						console.log(a.AssociationDataList.nodeDataList[j].alarmCount);
 					} else {
 						list[2] = parseInt(0);
 						alarm_list[2] = a.AssociationDataList.nodeDataList[j].alarmCase;
@@ -763,7 +767,7 @@ var apexDountChartOptions = {
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'RAM Usage Exceeded' ||
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'CPU Consumption is Excess' ||
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'Running Process is Exceeded') {
-						console.log(a.AssociationDataList.nodeDataList[j].alarmCount);
+//						console.log(a.AssociationDataList.nodeDataList[j].alarmCount);
 					} else {
 						list[3] = parseInt(0);
 						alarm_list[3] = a.AssociationDataList.nodeDataList[j].alarmCase;
@@ -779,7 +783,7 @@ var apexDountChartOptions = {
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'No Login History' ||
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'CPU Consumption is Excess' ||
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'Running Process is Exceeded') {
-						console.log(a.AssociationDataList.nodeDataList[j].alarmCount);
+//						console.log(a.AssociationDataList.nodeDataList[j].alarmCount);
 					} else {
 						list[4] = parseInt(0);
 						alarm_list[4] = a.AssociationDataList.nodeDataList[j].alarmCase;
@@ -795,7 +799,7 @@ var apexDountChartOptions = {
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'RAM Usage Exceeded' ||
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'No Login History' ||
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'Running Process is Exceeded') {
-						console.log(a.AssociationDataList.nodeDataList[j].alarmCount);
+//						console.log(a.AssociationDataList.nodeDataList[j].alarmCount);
 					} else {
 						list[5] = parseInt(0);
 						alarm_list[5] = a.AssociationDataList.nodeDataList[j].alarmCase;
@@ -811,7 +815,7 @@ var apexDountChartOptions = {
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'RAM Usage Exceeded' ||
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'CPU Consumption is Excess' ||
 						a.AssociationDataList.nodeDataList[j].alarmCase == 'No Login History') {
-						console.log(a.AssociationDataList.nodeDataList[j].alarmCount);
+//						console.log(a.AssociationDataList.nodeDataList[j].alarmCount);
 					} else {
 						list[6] = parseInt(0);
 						alarm_list[6] = a.AssociationDataList.nodeDataList[j].alarmCase;
@@ -826,7 +830,7 @@ var apexDountChartOptions = {
 		radar_list.push(radar_dict);
 	};
 	category_list.push(radar_list);
-	console.log(category_list);
+//	console.log(category_list);
 var apexRadarChartOptions = {
   chart: {
     width: "100%",
@@ -862,33 +866,25 @@ var apexRadarChartOptions = {
       },
     },
   },
-  colors: [
-    "rgba(" + app.color.themeRgb + ", .5)",
-    "rgba(" + app.color.themeRgb + ", .35)",
-    "rgba(" + app.color.themeRgb + ", .65)",
-    "rgba(" + app.color.themeRgb + ", .95)",
-    "rgba(" + app.color.themeRgb + ", .125)",
-  ],
+  colors: ["#ff9f0c", "#d08412", "#a16916", "#71501c", "#41341f"],
   markers: {
-    size: 4,
-    colors: [app.color.theme],
-    strokeColor: app.color.theme,
-    strokeWidth: 2,
+    size: 2,
+    colors: ["#ff9f0c", "#d08412", "#a16916", "#71501c", "#41341f"],
+    strokeColor: ["#ff9f0c", "#d08412", "#a16916", "#71501c", "#41341f"],
+    strokeWidth: 1,
   },
   tooltip: {
     theme: "dark",
     x: {
-      show: false,
+      show: true,
     },
     y: {
-      title: {
-        formatter: function (seriesName) {
-          return "";
-        },
-      },
-      formatter: (value) => {
-        return "" + value;
-      },
+        title: {
+            formatter: function (val) {
+                return '' + val
+            }
+		},
+		formatter: (value) => { return '' + value },
     },
   },
   xaxis: {
@@ -1244,7 +1240,7 @@ var handleRenderMap = function(worldMapData) {
 		map: 'world_mill',
 		normalizeFunction: 'polynomial',
 		hoverOpacity: 0.5,
-		hoverColor: false,
+		hoverColor: true,
 		zoomOnScroll: false,
 		series: {
 			regions: [{
@@ -1260,7 +1256,7 @@ var handleRenderMap = function(worldMapData) {
 			initial: {
 				fill: app.color.theme,
 				stroke: 'none',
-				"stroke-width": 1,
+				"stroke-width": 2,
 			}
 		},
 		regionStyle: {
@@ -1285,7 +1281,7 @@ var handleRenderMap = function(worldMapData) {
 // Failure Symptom Case - Korea Map chart
 //-----------------------------------------
 var handleRenderKoreaMap = function(worldMapData) {
-	var gdpData = { "AF": 16.63, "AL": 11.58, "DZ": 158.97, "AO": 85.81, "AG": 1.1, "AR": 351.02, "AM": 8.83, "AU": 1219.72, "AT": 366.26, "AZ": 52.17, "BS": 7.54, "BH": 21.73, "BD": 105.4, "BB": 3.96, "BY": 52.89, "BE": 461.33, "BZ": 1.43, "BJ": 6.49, "BT": 1.4, "BO": 19.18, "BA": 16.2, "BW": 12.5, "BR": 2023.53, "BN": 11.96, "BG": 44.84, "BF": 8.67, "BI": 1.47, "KH": 11.36, "CM": 21.88, "CA": 1563.66, "CV": 1.57, "CF": 2.11, "TD": 7.59, "CL": 199.18, "CN": 5745.13, "CO": 283.11, "KM": 0.56, "CD": 12.6, "CG": 11.88, "CR": 35.02, "CI": 22.38, "HR": 59.92, "CY": 22.75, "CZ": 195.23, "DK": 304.56, "DJ": 1.14, "DM": 0.38, "DO": 50.87, "EC": 61.49, "EG": 216.83, "SV": 21.8, "GQ": 14.55, "ER": 2.25, "EE": 19.22, "ET": 30.94, "FJ": 3.15, "FI": 231.98, "FR": 2555.44, "GA": 12.56, "GM": 1.04, "GE": 11.23, "DE": 3305.9, "GH": 18.06, "GR": 305.01, "GD": 0.65, "GT": 40.77, "GN": 4.34, "GW": 0.83, "GY": 2.2, "HT": 6.5, "HN": 15.34, "HK": 226.49, "HU": 132.28, "IS": 12.77, "IN": 1430.02, "ID": 695.06, "IR": 337.9, "IQ": 84.14, "IE": 204.14, "IL": 201.25, "IT": 2036.69, "JM": 13.74, "JP": 5390.9, "JO": 27.13, "KZ": 129.76, "KE": 32.42, "KI": 0.15, "KR": 986.26, "UNDEFINED": 5.73, "KW": 117.32, "KG": 4.44, "LA": 6.34, "LV": 23.39, "LB": 39.15, "LS": 1.8, "LR": 0.98, "LY": 77.91, "LT": 35.73, "LU": 52.43, "MK": 9.58, "MG": 8.33, "MW": 5.04, "MY": 218.95, "MV": 1.43, "ML": 9.08, "MT": 7.8, "MR": 3.49, "MU": 9.43, "MX": 1004.04, "MD": 5.36, "MN": 5.81, "ME": 3.88, "MA": 91.7, "MZ": 10.21, "MM": 35.65, "NA": 11.45, "NP": 15.11, "NL": 770.31, "NZ": 138, "NI": 6.38, "NE": 5.6, "NG": 206.66, "NO": 413.51, "OM": 53.78, "PK": 174.79, "PA": 27.2, "PG": 8.81, "PY": 17.17, "PE": 153.55, "PH": 189.06, "PL": 438.88, "PT": 223.7, "QA": 126.52, "RO": 158.39, "RU": 1476.91, "RW": 5.69, "WS": 0.55, "ST": 0.19, "SA": 434.44, "SN": 12.66, "RS": 38.92, "SC": 0.92, "SL": 1.9, "SG": 217.38, "SK": 86.26, "SI": 46.44, "SB": 0.67, "ZA": 354.41, "ES": 1374.78, "LK": 48.24, "KN": 0.56, "LC": 1, "VC": 0.58, "SD": 65.93, "SR": 3.3, "SZ": 3.17, "SE": 444.59, "CH": 522.44, "SY": 59.63, "TW": 426.98, "TJ": 5.58, "TZ": 22.43, "TH": 312.61, "TL": 0.62, "TG": 3.07, "TO": 0.3, "TT": 21.2, "TN": 43.86, "TR": 729.05, "TM": 0, "UG": 17.12, "UA": 136.56, "AE": 239.65, "GB": 2258.57, "US": 14624.18, "UY": 40.71, "UZ": 37.72, "VU": 0.72, "VE": 285.21, "VN": 101.99, "YE": 30.02, "ZM": 15.69, "ZW": 5.57};
+	var gdpData2 = { "AF": 16.63, "AL": 11.58, "DZ": 158.97, "AO": 85.81, "AG": 1.1, "AR": 351.02, "AM": 8.83, "AU": 1219.72, "AT": 366.26, "AZ": 52.17, "BS": 7.54, "BH": 21.73, "BD": 105.4, "BB": 3.96, "BY": 52.89, "BE": 461.33, "BZ": 1.43, "BJ": 6.49, "BT": 1.4, "BO": 19.18, "BA": 16.2, "BW": 12.5, "BR": 2023.53, "BN": 11.96, "BG": 44.84, "BF": 8.67, "BI": 1.47, "KH": 11.36, "CM": 21.88, "CA": 1563.66, "CV": 1.57, "CF": 2.11, "TD": 7.59, "CL": 199.18, "CN": 5745.13, "CO": 283.11, "KM": 0.56, "CD": 12.6, "CG": 11.88, "CR": 35.02, "CI": 22.38, "HR": 59.92, "CY": 22.75, "CZ": 195.23, "DK": 304.56, "DJ": 1.14, "DM": 0.38, "DO": 50.87, "EC": 61.49, "EG": 216.83, "SV": 21.8, "GQ": 14.55, "ER": 2.25, "EE": 19.22, "ET": 30.94, "FJ": 3.15, "FI": 231.98, "FR": 2555.44, "GA": 12.56, "GM": 1.04, "GE": 11.23, "DE": 3305.9, "GH": 18.06, "GR": 305.01, "GD": 0.65, "GT": 40.77, "GN": 4.34, "GW": 0.83, "GY": 2.2, "HT": 6.5, "HN": 15.34, "HK": 226.49, "HU": 132.28, "IS": 12.77, "IN": 1430.02, "ID": 695.06, "IR": 337.9, "IQ": 84.14, "IE": 204.14, "IL": 201.25, "IT": 2036.69, "JM": 13.74, "JP": 5390.9, "JO": 27.13, "KZ": 129.76, "KE": 32.42, "KI": 0.15, "KR": 986.26, "UNDEFINED": 5.73, "KW": 117.32, "KG": 4.44, "LA": 6.34, "LV": 23.39, "LB": 39.15, "LS": 1.8, "LR": 0.98, "LY": 77.91, "LT": 35.73, "LU": 52.43, "MK": 9.58, "MG": 8.33, "MW": 5.04, "MY": 218.95, "MV": 1.43, "ML": 9.08, "MT": 7.8, "MR": 3.49, "MU": 9.43, "MX": 1004.04, "MD": 5.36, "MN": 5.81, "ME": 3.88, "MA": 91.7, "MZ": 10.21, "MM": 35.65, "NA": 11.45, "NP": 15.11, "NL": 770.31, "NZ": 138, "NI": 6.38, "NE": 5.6, "NG": 206.66, "NO": 413.51, "OM": 53.78, "PK": 174.79, "PA": 27.2, "PG": 8.81, "PY": 17.17, "PE": 153.55, "PH": 189.06, "PL": 438.88, "PT": 223.7, "QA": 126.52, "RO": 158.39, "RU": 1476.91, "RW": 5.69, "WS": 0.55, "ST": 0.19, "SA": 434.44, "SN": 12.66, "RS": 38.92, "SC": 0.92, "SL": 1.9, "SG": 217.38, "SK": 86.26, "SI": 46.44, "SB": 0.67, "ZA": 354.41, "ES": 1374.78, "LK": 48.24, "KN": 0.56, "LC": 1, "VC": 0.58, "SD": 65.93, "SR": 3.3, "SZ": 3.17, "SE": 444.59, "CH": 522.44, "SY": 59.63, "TW": 426.98, "TJ": 5.58, "TZ": 22.43, "TH": 312.61, "TL": 0.62, "TG": 3.07, "TO": 0.3, "TT": 21.2, "TN": 43.86, "TR": 729.05, "TM": 0, "UG": 17.12, "UA": 136.56, "AE": 239.65, "GB": 2258.57, "US": 14624.18, "UY": 40.71, "UZ": 37.72, "VU": 0.72, "VE": 285.21, "VN": 101.99, "YE": 30.02, "ZM": 15.69, "ZW": 5.57};
 	$('#korea-map').vectorMap({
 		map: 'kr_mill',
 		normalizeFunction: 'polynomial',
@@ -1331,7 +1327,7 @@ var handleRenderKoreaMap = function(worldMapData) {
 // Failure Symptom Case - Seongnam Map chart
 //------------------------------------------
 //var handleRenderSeongnamMap = function() {
-//	var gdpData = { "AF": 16.63, "AL": 11.58, "DZ": 158.97, "AO": 85.81, "AG": 1.1, "AR": 351.02, "AM": 8.83, "AU": 1219.72, "AT": 366.26, "AZ": 52.17, "BS": 7.54, "BH": 21.73, "BD": 105.4, "BB": 3.96, "BY": 52.89, "BE": 461.33, "BZ": 1.43, "BJ": 6.49, "BT": 1.4, "BO": 19.18, "BA": 16.2, "BW": 12.5, "BR": 2023.53, "BN": 11.96, "BG": 44.84, "BF": 8.67, "BI": 1.47, "KH": 11.36, "CM": 21.88, "CA": 1563.66, "CV": 1.57, "CF": 2.11, "TD": 7.59, "CL": 199.18, "CN": 5745.13, "CO": 283.11, "KM": 0.56, "CD": 12.6, "CG": 11.88, "CR": 35.02, "CI": 22.38, "HR": 59.92, "CY": 22.75, "CZ": 195.23, "DK": 304.56, "DJ": 1.14, "DM": 0.38, "DO": 50.87, "EC": 61.49, "EG": 216.83, "SV": 21.8, "GQ": 14.55, "ER": 2.25, "EE": 19.22, "ET": 30.94, "FJ": 3.15, "FI": 231.98, "FR": 2555.44, "GA": 12.56, "GM": 1.04, "GE": 11.23, "DE": 3305.9, "GH": 18.06, "GR": 305.01, "GD": 0.65, "GT": 40.77, "GN": 4.34, "GW": 0.83, "GY": 2.2, "HT": 6.5, "HN": 15.34, "HK": 226.49, "HU": 132.28, "IS": 12.77, "IN": 1430.02, "ID": 695.06, "IR": 337.9, "IQ": 84.14, "IE": 204.14, "IL": 201.25, "IT": 2036.69, "JM": 13.74, "JP": 5390.9, "JO": 27.13, "KZ": 129.76, "KE": 32.42, "KI": 0.15, "KR": 986.26, "UNDEFINED": 5.73, "KW": 117.32, "KG": 4.44, "LA": 6.34, "LV": 23.39, "LB": 39.15, "LS": 1.8, "LR": 0.98, "LY": 77.91, "LT": 35.73, "LU": 52.43, "MK": 9.58, "MG": 8.33, "MW": 5.04, "MY": 218.95, "MV": 1.43, "ML": 9.08, "MT": 7.8, "MR": 3.49, "MU": 9.43, "MX": 1004.04, "MD": 5.36, "MN": 5.81, "ME": 3.88, "MA": 91.7, "MZ": 10.21, "MM": 35.65, "NA": 11.45, "NP": 15.11, "NL": 770.31, "NZ": 138, "NI": 6.38, "NE": 5.6, "NG": 206.66, "NO": 413.51, "OM": 53.78, "PK": 174.79, "PA": 27.2, "PG": 8.81, "PY": 17.17, "PE": 153.55, "PH": 189.06, "PL": 438.88, "PT": 223.7, "QA": 126.52, "RO": 158.39, "RU": 1476.91, "RW": 5.69, "WS": 0.55, "ST": 0.19, "SA": 434.44, "SN": 12.66, "RS": 38.92, "SC": 0.92, "SL": 1.9, "SG": 217.38, "SK": 86.26, "SI": 46.44, "SB": 0.67, "ZA": 354.41, "ES": 1374.78, "LK": 48.24, "KN": 0.56, "LC": 1, "VC": 0.58, "SD": 65.93, "SR": 3.3, "SZ": 3.17, "SE": 444.59, "CH": 522.44, "SY": 59.63, "TW": 426.98, "TJ": 5.58, "TZ": 22.43, "TH": 312.61, "TL": 0.62, "TG": 3.07, "TO": 0.3, "TT": 21.2, "TN": 43.86, "TR": 729.05, "TM": 0, "UG": 17.12, "UA": 136.56, "AE": 239.65, "GB": 2258.57, "US": 14624.18, "UY": 40.71, "UZ": 37.72, "VU": 0.72, "VE": 285.21, "VN": 101.99, "YE": 30.02, "ZM": 15.69, "ZW": 5.57};
+//	var gdpData3 = { "AF": 16.63, "AL": 11.58, "DZ": 158.97, "AO": 85.81, "AG": 1.1, "AR": 351.02, "AM": 8.83, "AU": 1219.72, "AT": 366.26, "AZ": 52.17, "BS": 7.54, "BH": 21.73, "BD": 105.4, "BB": 3.96, "BY": 52.89, "BE": 461.33, "BZ": 1.43, "BJ": 6.49, "BT": 1.4, "BO": 19.18, "BA": 16.2, "BW": 12.5, "BR": 2023.53, "BN": 11.96, "BG": 44.84, "BF": 8.67, "BI": 1.47, "KH": 11.36, "CM": 21.88, "CA": 1563.66, "CV": 1.57, "CF": 2.11, "TD": 7.59, "CL": 199.18, "CN": 5745.13, "CO": 283.11, "KM": 0.56, "CD": 12.6, "CG": 11.88, "CR": 35.02, "CI": 22.38, "HR": 59.92, "CY": 22.75, "CZ": 195.23, "DK": 304.56, "DJ": 1.14, "DM": 0.38, "DO": 50.87, "EC": 61.49, "EG": 216.83, "SV": 21.8, "GQ": 14.55, "ER": 2.25, "EE": 19.22, "ET": 30.94, "FJ": 3.15, "FI": 231.98, "FR": 2555.44, "GA": 12.56, "GM": 1.04, "GE": 11.23, "DE": 3305.9, "GH": 18.06, "GR": 305.01, "GD": 0.65, "GT": 40.77, "GN": 4.34, "GW": 0.83, "GY": 2.2, "HT": 6.5, "HN": 15.34, "HK": 226.49, "HU": 132.28, "IS": 12.77, "IN": 1430.02, "ID": 695.06, "IR": 337.9, "IQ": 84.14, "IE": 204.14, "IL": 201.25, "IT": 2036.69, "JM": 13.74, "JP": 5390.9, "JO": 27.13, "KZ": 129.76, "KE": 32.42, "KI": 0.15, "KR": 986.26, "UNDEFINED": 5.73, "KW": 117.32, "KG": 4.44, "LA": 6.34, "LV": 23.39, "LB": 39.15, "LS": 1.8, "LR": 0.98, "LY": 77.91, "LT": 35.73, "LU": 52.43, "MK": 9.58, "MG": 8.33, "MW": 5.04, "MY": 218.95, "MV": 1.43, "ML": 9.08, "MT": 7.8, "MR": 3.49, "MU": 9.43, "MX": 1004.04, "MD": 5.36, "MN": 5.81, "ME": 3.88, "MA": 91.7, "MZ": 10.21, "MM": 35.65, "NA": 11.45, "NP": 15.11, "NL": 770.31, "NZ": 138, "NI": 6.38, "NE": 5.6, "NG": 206.66, "NO": 413.51, "OM": 53.78, "PK": 174.79, "PA": 27.2, "PG": 8.81, "PY": 17.17, "PE": 153.55, "PH": 189.06, "PL": 438.88, "PT": 223.7, "QA": 126.52, "RO": 158.39, "RU": 1476.91, "RW": 5.69, "WS": 0.55, "ST": 0.19, "SA": 434.44, "SN": 12.66, "RS": 38.92, "SC": 0.92, "SL": 1.9, "SG": 217.38, "SK": 86.26, "SI": 46.44, "SB": 0.67, "ZA": 354.41, "ES": 1374.78, "LK": 48.24, "KN": 0.56, "LC": 1, "VC": 0.58, "SD": 65.93, "SR": 3.3, "SZ": 3.17, "SE": 444.59, "CH": 522.44, "SY": 59.63, "TW": 426.98, "TJ": 5.58, "TZ": 22.43, "TH": 312.61, "TL": 0.62, "TG": 3.07, "TO": 0.3, "TT": 21.2, "TN": 43.86, "TR": 729.05, "TM": 0, "UG": 17.12, "UA": 136.56, "AE": 239.65, "GB": 2258.57, "US": 14624.18, "UY": 40.71, "UZ": 37.72, "VU": 0.72, "VE": 285.21, "VN": 101.99, "YE": 30.02, "ZM": 15.69, "ZW": 5.57};
 //	$('#seongnamMap').vectorMap({
 //		map: 'sn_mill',
 //		normalizeFunction: 'polynomial',
@@ -1402,7 +1398,7 @@ var handleRenderKoreaMap = function(worldMapData) {
 
 
 function seongnamMap(worldMapData, seongnamNetwork) {
-	var width = 800, height = 376.92;
+	var width = 600, height = 376.92;
 	var svg = d3.select("#seongnam-map").append("svg").attr("width", width).attr("height", height).attr("viewBox", "0 0 879.5 376.92").style("margin-left", '1%');
 	var map = svg.append("g").attr("id", "map"), places = svg.append("g").attr("id", "places");
 	var projection = d3.geo.mercator().center([127.1094211519, 37.399]).scale(120000).translate([width / 2, height / 2]);
@@ -1500,7 +1496,7 @@ $(document).ready(function() {
 	seongnamMap(worldMapData, seongnamNetwork);
 
 	document.addEventListener('theme-reload', function() {
-	$('[data-render="apexchart"], #apexRadarChart #world-map ').empty();
+	$('[data-render="apexchart"], #apexRadarChart #world-map #korea-map #seongnamMap').empty();
 		handleRenderChart();
 		handleRenderMap(worldMapData);
 		handleRenderKoreaMap(worldMapData);
