9e3d39066cc30ca5e30964621becac08772d1721
mjKim <mendoda20@gmail.com>
2022-10-27 10:04:02 +0900

    1. app.min.css > ol, ul, li, p 기존 템플릿 css 속성 margin-bottom 값 주석 처리
    2. report_monthly.html
    > 코드 재정렬
    
    3. table-plugins.demo.js
    > reportMonthTable -> reportWeeklyTable로 수정

--- static/assets/js/demo/table-plugins.demo.js
+++ static/assets/js/demo/table-plugins.demo.js
@@ -5,174 +5,174 @@ Author: Sean Ngu
 Website: http://www.seantheme.com/hud/
 */
 
-var handleRenderReportTableData = function() {
-    var reportTable = $('#reportDailyTable, #reportMonthTable, #reportMonthlyTable').DataTable({
-	    destroy: true,
+var handleRenderReportTableData = function () {
+	var reportTable = $('#reportDailyTable, #reportWeeklyTable, #reportMonthlyTable').DataTable({
+		destroy: true,
 		dom: "<'row mb-3'<'col-md-4 mb-3 mb-md-0'l><'col-md-8 text-right'<'d-flex justify-content-end'fB>>>t<'row align-items-center'<'mr-auto col-md-6 mb-3 mb-md-0 mt-n2 'i><'mb-0 col-md-6'p>>",
-		lengthMenu: [ 10, 20, 30, 40, 50 ],
+		lengthMenu: [10, 20, 30, 40, 50],
 		responsive: true,
 		searching: true,
-        language: {
-		    "decimal":        "",
-			"info":           "현재 _START_ - _END_건 / 전체 _TOTAL_건",
-			"infoEmpty":      "데이터가 없습니다.",
-			"emptyTable":     "데이터가 없습니다.",
-			"thousands":      ",",
-			"lengthMenu":     "페이지당 _MENU_ 개씩 보기",
+		language: {
+			"decimal": "",
+			"info": "현재 _START_ - _END_건 / 전체 _TOTAL_건",
+			"infoEmpty": "데이터가 없습니다.",
+			"emptyTable": "데이터가 없습니다.",
+			"thousands": ",",
+			"lengthMenu": "페이지당 _MENU_ 개씩 보기",
 			"loadingRecords": "로딩 중입니다.",
-			"processing":     "",
-			"zeroRecords":    "검색 결과 없음",
+			"processing": "",
+			"zeroRecords": "검색 결과 없음",
 			"paginate": {
-				"first":      "처음",
-				"last":       "끝",
-				"next":       "다음",
-				"previous":   "이전"
+				"first": "처음",
+				"last": "끝",
+				"next": "다음",
+				"previous": "이전"
 			},
-			"search":         "검색:",
-			"infoFiltered":   "(전체 _MAX_ 건 중 검색결과)",
-			"infoPostFix":    "",
-        },
+			"search": "검색:",
+			"infoFiltered": "(전체 _MAX_ 건 중 검색결과)",
+			"infoPostFix": "",
+		},
 	});
 };
 
-var handleRenderWeakTableData = function() {
+var handleRenderWeakTableData = function () {
 	var weakTable = $('#weakTable-windows, #weakTable-unix').DataTable({
 		dom: "<'row mb-3'<'col-md-4 mb-3 mb-md-0'l><'col-md-8 text-right'<'d-flex justify-content-end'fB>>>t<'row align-items-center'<'mr-auto col-md-6 mb-3 mb-md-0 mt-n2 'i><'mb-0 col-md-6'p>>",
-		lengthMenu: [ 10, 20, 30, 40, 50 ],
+		lengthMenu: [10, 20, 30, 40, 50],
 		responsive: true,
 		searching: true,
 		autoWidth: false,
 		ordering: false,
-	    columnDefs: [
+		columnDefs: [
 
-	        {width: "22%", target: [2] },
-	        {width: "65%", target: [3] },
+			{ width: "22%", target: [2] },
+			{ width: "65%", target: [3] },
 
-	    ],
-        language: {
-		    "decimal":        "",
-			"info":           "현재 _START_ - _END_건 / 전체 _TOTAL_건",
-			"infoEmpty":      "데이터가 없습니다.",
-			"emptyTable":     "데이터가 없습니다.",
-			"thousands":      ",",
-			"lengthMenu":     "페이지당 _MENU_ 개씩 보기",
+		],
+		language: {
+			"decimal": "",
+			"info": "현재 _START_ - _END_건 / 전체 _TOTAL_건",
+			"infoEmpty": "데이터가 없습니다.",
+			"emptyTable": "데이터가 없습니다.",
+			"thousands": ",",
+			"lengthMenu": "페이지당 _MENU_ 개씩 보기",
 			"loadingRecords": "로딩 중입니다.",
-			"processing":     "",
-			"zeroRecords":    "검색 결과 없음",
+			"processing": "",
+			"zeroRecords": "검색 결과 없음",
 			"paginate": {
-				"first":      "처음",
-				"last":       "끝",
-				"next":       "다음",
-				"previous":   "이전"
+				"first": "처음",
+				"last": "끝",
+				"next": "다음",
+				"previous": "이전"
 			},
-			"search":         "검색:",
-			"infoFiltered":   "(전체 _MAX_ 건 중 검색결과)",
-			"infoPostFix":    "",
-        },
+			"search": "검색:",
+			"infoFiltered": "(전체 _MAX_ 건 중 검색결과)",
+			"infoPostFix": "",
+		},
 	});
-    weakTable.draw();
+	weakTable.draw();
 };
 
-var handleRenderWeakDetailTableData = function() {
+var handleRenderWeakDetailTableData = function () {
 	var weakDetailtable = $('#weakTableDetail').DataTable({
 		dom: "<'row mb-3'<'col-md-4 mb-3 mb-md-0'l><'col-md-8 text-right'<'d-flex justify-content-end'fB>>>t<'row align-items-center'<'mr-auto col-md-6 mb-3 mb-md-0 mt-n2 'i><'mb-0 col-md-6'p>>",
-		lengthMenu: [ 10, 20, 30, 40, 50 ],
+		lengthMenu: [10, 20, 30, 40, 50],
 		responsive: true,
 		searching: true,
 		autoWidth: false,
 		ordering: false,
-	    columnDefs: [
-	        {width: "5%", target: [0] },
-	        {width: "15%", target: [1] },
-	        {width: "16%", target: [2] },
-	        {width: "20%", target: [3] },
-	        {width: "14%", target: [4] },
-	        {width: "10%", target: [5] },
-	        {width: "15%", target: [6] },
-	        {width: "5%", target: [7] }
-	    ],
-        language: {
-		    "decimal":        "",
-			"info":           "현재 _START_ - _END_건 / 전체 _TOTAL_건",
-			"infoEmpty":      "데이터가 없습니다.",
-			"emptyTable":     "데이터가 없습니다.",
-			"thousands":      ",",
-			"lengthMenu":     "페이지당 _MENU_ 개씩 보기",
+		columnDefs: [
+			{ width: "5%", target: [0] },
+			{ width: "15%", target: [1] },
+			{ width: "16%", target: [2] },
+			{ width: "20%", target: [3] },
+			{ width: "14%", target: [4] },
+			{ width: "10%", target: [5] },
+			{ width: "15%", target: [6] },
+			{ width: "5%", target: [7] }
+		],
+		language: {
+			"decimal": "",
+			"info": "현재 _START_ - _END_건 / 전체 _TOTAL_건",
+			"infoEmpty": "데이터가 없습니다.",
+			"emptyTable": "데이터가 없습니다.",
+			"thousands": ",",
+			"lengthMenu": "페이지당 _MENU_ 개씩 보기",
 			"loadingRecords": "로딩 중입니다.",
-			"processing":     "",
-			"zeroRecords":    "검색 결과 없음",
+			"processing": "",
+			"zeroRecords": "검색 결과 없음",
 			"paginate": {
-				"first":      "처음",
-				"last":       "끝",
-				"next":       "다음",
-				"previous":   "이전"
+				"first": "처음",
+				"last": "끝",
+				"next": "다음",
+				"previous": "이전"
 			},
-			"search":         "검색:",
-			"infoFiltered":   "(전체 _MAX_ 건 중 검색결과)",
-			"infoPostFix":    "",
-        },
+			"search": "검색:",
+			"infoFiltered": "(전체 _MAX_ 건 중 검색결과)",
+			"infoPostFix": "",
+		},
 	});
-    weakDetailtable.draw();
+	weakDetailtable.draw();
 };
 
-var handleRenderWeakDetailModalTableData = function() {
+var handleRenderWeakDetailModalTableData = function () {
 	var weakDetailModaltable = $('#weakTableDetail_modal').DataTable({
 		dom: "<'row mb-3'<'col-md-4 mb-3 mb-md-0'l><'col-md-8 text-right'<'d-flex justify-content-end'fB>>>t<'row align-items-center'<'mr-auto col-md-6 mb-3 mb-md-0 mt-n2 'i><'mb-0 col-md-6'p>>",
-		lengthMenu: [ 10, 20, 30, 40, 50 ],
+		lengthMenu: [10, 20, 30, 40, 50],
 		responsive: true,
 		searching: true,
 		autoWidth: false,
 		ordering: false,
-	    columnDefs: [
-	        {width: "5%", target: [0] },
-	        {width: "85%", target: [1] },
-	        {width: "5%", target: [2] },
-	        {width: "5%", target: [3] },
-	    ],
+		columnDefs: [
+			{ width: "5%", target: [0] },
+			{ width: "85%", target: [1] },
+			{ width: "5%", target: [2] },
+			{ width: "5%", target: [3] },
+		],
 		language: {
-			"info":           "현재 _START_ - _END_건 / 전체 _TOTAL_건",
-			"infoEmpty":      "데이터가 없습니다.",
-			"emptyTable":     "데이터가 없습니다.",
-			"thousands":      ",",
-			"lengthMenu":     "페이지당 _MENU_ 개씩 보기",
+			"info": "현재 _START_ - _END_건 / 전체 _TOTAL_건",
+			"infoEmpty": "데이터가 없습니다.",
+			"emptyTable": "데이터가 없습니다.",
+			"thousands": ",",
+			"lengthMenu": "페이지당 _MENU_ 개씩 보기",
 			"loadingRecords": "로딩 중입니다.",
-			"processing":     "",
-			"zeroRecords":    "검색 결과 없음",
+			"processing": "",
+			"zeroRecords": "검색 결과 없음",
 			"paginate": {
-				"first":      "처음",
-				"last":       "끝",
-				"next":       "다음",
-				"previous":   "이전"
+				"first": "처음",
+				"last": "끝",
+				"next": "다음",
+				"previous": "이전"
 			},
-			"search":         "검색:",
-			"infoFiltered":   "(전체 _MAX_ 건 중 검색결과)",
-			"infoPostFix":    "",
-        },
+			"search": "검색:",
+			"infoFiltered": "(전체 _MAX_ 건 중 검색결과)",
+			"infoPostFix": "",
+		},
 	});
 
-		$("#weakTableDetail_modal_filter.dataTables_filter").append($("#weak-box"));
+	$("#weakTableDetail_modal_filter.dataTables_filter").append($("#weak-box"));
 
 	var categoryIndex = 0;
 	$("#weakTableDetail_modal th").each(function (i) {
-	  if ($($(this)).html() == "상태") {
-		categoryIndex = i; return false;
-	  }
+		if ($($(this)).html() == "상태") {
+			categoryIndex = i; return false;
+		}
 	});
 	$.fn.dataTable.ext.search.push(
-        function (settings, data, dataIndex) {
-          var selectedItem = $('#weak-box').val()
-          var category = data[categoryIndex];
-          if (selectedItem === "" || category.includes(selectedItem)) {
-            return true;
-          }
-		  return false;
-        }
-      );
+		function (settings, data, dataIndex) {
+			var selectedItem = $('#weak-box').val()
+			var category = data[categoryIndex];
+			if (selectedItem === "" || category.includes(selectedItem)) {
+				return true;
+			}
+			return false;
+		}
+	);
 
-	  $("#weak-box").change(function (e) {
+	$("#weak-box").change(function (e) {
 		weakDetailModaltable.draw();
-      });
-    weakDetailModaltable.draw();
+	});
+	weakDetailModaltable.draw();
 };
 
 
@@ -182,11 +182,11 @@ var handleRenderWeakDetailModalTableData = function() {
 
 /* Controller weakBox
 ------------------------------------------------ */
-$(document).ready(function() {
-    handleRenderReportTableData();
+$(document).ready(function () {
+	handleRenderReportTableData();
 	handleRenderWeakTableData();
-    handleRenderWeakDetailTableData();
-    handleRenderWeakDetailModalTableData();
+	handleRenderWeakDetailTableData();
+	handleRenderWeakDetailModalTableData();
 
 
 });
