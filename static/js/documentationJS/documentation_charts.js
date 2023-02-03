2e6023306e7a241428cea4e3fa858ce078f2f5dc
smKim <smkim@xionits.com>
2023-02-03 17:12:08 +0900

    ※수정 사항 라인차트 완료

--- static/js/documentationJS/documentation_charts.js
+++ static/js/documentationJS/documentation_charts.js
@@ -81,46 +81,69 @@ var handleRenderGuideApexChart = function () {
 
 // BEGIN Guide_columnChart
   var DocumentColumnChartOptions = {
-      series: [{
-      data: [11, 9, 7, 5, 3]
-    }],
-      chart: {
+    chart: {
       height: 120,
       type: 'bar',
         toolbar: {
           show: false
-      },
-      events: {
-        click: function(chart, w, e) {
-          // console.log(chart, w, e)
-        }
-      }
+        },
     },
     plotOptions: {
       bar: {
-        columnWidth: '45%',
+        columnWidth: '70%',
         distributed: true,
       }
-    },
-    dataLabels: {
-      enabled: false
     },
     legend: {
       show: false
     },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+        show: true,
+        width: 1,
+        colors: ['transparent']
+    },
+    colors: ["#f39c12", "#fdb43f", "#ffc365", "#c58a2e", "#e6b567"],
+    series: [{
+      data: [11, 9, 7, 5, 3]
+    }],
+    grid: {
+        show: true
+    },
     xaxis: {
-      categories: [
-        '192.168.1',
-        '192.168.0',
-        '192.168.14',
-        '192.168.64',
-        '192.168.5',
-      ],
+      categories: ['192.168.1', '192.168.0', '192.168.14', '192.168.64', '192.168.5'],
       labels: {
+        show: true,
         style: {
-          fontSize: '12px'
+          colors: '#fff',
+          fontSize: '9px',
+          cssClass: 'apexcharts-xaxis-label',
         }
       }
+    },
+    yaxis: {
+      labels: {
+        show: true,
+      }
+    },
+	fill: {
+	  opacity: 1
+	},
+    tooltip: {
+      theme: 'dark',
+        x: {
+          show: true,
+        },
+		y: {
+	      title: {
+		    formatter: function (seriesName) {
+		    return ''
+			}
+		  },
+	    formatter: (value) => { return '' + value },
+	  }
     }
   }
 
@@ -133,29 +156,70 @@ var handleRenderGuideApexChart = function () {
 //--------------------------------------------------------------------------
   // BEGIN Guide_barChart
   var DocumentBarChartOptions = {
-    series: [{
-      data: [8, 4, 2]
-    }],
       chart: {
-      type: 'bar',
-      width: 500,
-      height: 150,
+        height: 120,
+        type: 'bar',
         toolbar: {
-        show: false
+          show: false
       },
     },
     plotOptions: {
       bar: {
-        borderRadius: 4,
         horizontal: true,
-      }
+        distributed: true
+      },
+    },
+    legend : {
+      show: false
     },
     dataLabels: {
       enabled: false
     },
+    stroke: {
+      show: true,
+      width: 1,
+      colors: ['transparent']
+    },
+    colors: ["#f39c12", "#fdb43f", "#ffc365"],
+    series: [{
+      data: [8, 6, 4]
+    }],
+    grid: {
+      show: true
+    },
     xaxis: {
-      categories: ['LG Electronics', 'HP', 'To Be FilledBy O.E.M'
-      ],
+      categories: ['LG Electronics', 'SAMSUNG ELECTRONICS CO., LED.', 'To Be Filled By O.E.M.'],
+        labels: {
+          show: true,
+          style: {
+            colors: '#fff',
+              fontSize: '9px',
+              cssClass: 'apexcharts-xaxis-label',
+          },
+        offsetX: 30,
+        },
+    },
+    yaxis: {
+        labels: {
+            show: true,
+        }
+    },
+    fill: {
+        opacity: 1
+    },
+    tooltip: {
+      theme: 'dark',
+      x: {
+        show: true,
+      },
+      y: {
+            title: {
+                formatter: function (seriesName) {
+                    return ''
+                }
+            },
+            formatter: (value) => { return '' + value },
+      }
     }
   };
   var DocumentBarChart = new ApexCharts(
@@ -168,29 +232,63 @@ var handleRenderGuideApexChart = function () {
   // BEGIN Guide_LineChart
   var DocumentLineChartOptions = {
     series: [{
-      data: [8, 4, 2]
+      name: "virtual",
+      data: [8, 9, 10, 7, 8, 12, 16, 20, 18, 14, 8, 10, 16, 20, 25, 18,
+       10, 8, 3, 10, 19, 21, 27, 20, 23, 27 ,30, 34, 39, 44]
+    },
+    {
+      name: "physical",
+      data: [46, 42, 39, 36, 30, 32, 26, 28, 31, 10, 6, 14, 22, 26, 19,
+      15, 20, 26, 22, 19, 6, 10, 17, 10, 6, 18, 26, 20, 16, 25]
     }],
-      chart: {
-      type: 'bar',
-      width: 500,
-      height: 150,
-        toolbar: {
+    chart: {
+      height: 145,
+      type: 'line',
+      toolbar: {
         show: false
       },
-    },
-    plotOptions: {
-      bar: {
-        borderRadius: 4,
-        horizontal: true,
+      events: {
+        mounted: (chart) => {
+          chart.windowResizeHandler();
+        }
       }
     },
+    colors: ['rgba(' + app.color.themeRgb + ', .95)', 'rgba(' + app.color.themeRgb + ', .30)'],
     dataLabels: {
-      enabled: false
+      enabled: false,
+    },
+    legend : {
+      show: false
+    },
+    stroke: {
+      curve: 'smooth',
+      width: 3
+    },
+    grid: {
+      row: {
+        colors: ['rgba(' + app.color.whiteRgb + ', .25)', 'transparent'], // takes an array which will be repeated on columns
+        opacity: 0.5
+      }
+    },
+    markers: {
+      size: 1
     },
     xaxis: {
-      categories: ['LG Electronics', 'HP', 'To Be FilledBy O.E.M'
-      ],
-    }
+      categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11',
+      '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23',
+      '24', '25', '26', '27', '28', '29', '30'],
+      tooltip: {
+        enabled: false,
+      },
+    },
+    yaxis: {
+      labels: {
+        show: true,
+        formatter: function (val) {
+          return Math.round(val);
+        }
+      }
+    },
   };
   var DocumentLineChart = new ApexCharts(
     document.querySelector("#DocumentLineChart"),
@@ -201,7 +299,14 @@ var handleRenderGuideApexChart = function () {
 //--------------------------------------------------------------------------
   // BEGIN Guide_PieChart
   var DocumentPieChartOptions = {
-    series: [31,9],
+     series: [{
+      name: "Windows",
+      data: [31]
+    },
+    {
+      name: "Linux",
+      data: [9]
+    }],
       chart: {
       width: 380,
       type: 'pie',
@@ -217,7 +322,21 @@ var handleRenderGuideApexChart = function () {
           position: 'bottom'
         }
       }
-    }]
+    }],
+    tooltip: {
+      theme: 'dark',
+      x: {
+        show: true,
+      },
+      y: {
+            title: {
+                formatter: function (labelsname) {
+                    return ''
+                }
+            },
+            formatter: (value) => { return '' + value },
+      }
+    }
   };
   var DocumentPieChart = new ApexCharts(
     document.querySelector("#DocumentPieChart"),
@@ -242,7 +361,7 @@ var handleRenderGuideApexChart = function () {
           position: 'bottom'
         }
       }
-    }]
+    }],
   };
   var DocumentDonutChart = new ApexCharts(
     document.querySelector("#DocumentDonutChart"),
@@ -251,30 +370,137 @@ var handleRenderGuideApexChart = function () {
   DocumentDonutChart.render();
   // END Guide_DonutChart
 //--------------------------------------------------------------------------
-  // BEGIN Guide_DonutChart
-  var DocumentCircleChartOptions = {
-    series: [44, 55, 41, 17, 15],
+  // BEGIN Guide_CircleChart
+  var DocumentGaugeChartOptions = {
+    series: [75],
     chart: {
-     type: 'donut',
+      height: 350,
+      type: 'radialBar',
+      toolbar: {
+        show: true
+      }
     },
-    responsive: [{
-      breakpoint: 480,
-      options: {
-        chart: {
-          width: 200
+    plotOptions: {
+      radialBar: {
+        startAngle: -135,
+        endAngle: 225,
+        hollow: {
+          margin: 0,
+          size: '70%',
+          background: '#fff',
+          image: undefined,
+          imageOffsetX: 0,
+          imageOffsetY: 0,
+          position: 'front',
+          dropShadow: {
+            enabled: true,
+            top: 3,
+            left: 0,
+            blur: 4,
+            opacity: 0.24
+          }
         },
-        legend: {
-          position: 'bottom'
+        track: {
+          background: '#fff',
+          strokeWidth: '67%',
+          margin: 0, // margin is in pixels
+          dropShadow: {
+            enabled: true,
+            top: -3,
+            left: 0,
+            blur: 4,
+            opacity: 0.35
+          }
+        },
+        dataLabels: {
+          show: true,
+          name: {
+            offsetY: -10,
+            show: true,
+            color: '#888',
+            fontSize: '17px'
+          },
+          value: {
+            formatter: function(val) {
+              return parseInt(val);
+            },
+            color: '#111',
+            fontSize: '36px',
+            show: true,
+          }
         }
       }
-    }]
+    },
+    fill: {
+      type: 'gradient',
+      gradient: {
+        shade: 'dark',
+        type: 'horizontal',
+        shadeIntensity: 0.5,
+        gradientToColors: ['#ABE5A1'],
+        inverseColors: true,
+        opacityFrom: 1,
+        opacityTo: 1,
+        stops: [0, 100]
+      }
+    },
+    stroke: {
+      lineCap: 'round'
+    },
+    labels: ['0'],
   };
-  var DocumentCircleChart = new ApexCharts(
-    document.querySelector("#DocumentCircleChart"),
-    DocumentCircleChartOptions
+    var DocumentGaugeChart = new ApexCharts(
+      document.querySelector("#DocumentGaugeChart"),
+    DocumentGaugeChartOptions
   );
-  DocumentCircleChart.render();
-  // END Guide_DonutChart
+  DocumentGaugeChart.render();
+//--------------------------------------------------------------------------
+  // BEGIN Guide_CircleChart
+  var DocumentRaderChartOptions = {
+    series: [{
+      name: '192.168.1',
+      data: [1, 2, 3, 4, 5, 6, 7],
+    }, {
+      name: '192.168.14',
+      data: [7, 6, 5, 4, 3, 2, 1],
+    }, {
+      name: '192.168.0',
+      data: [2, 4, 6, 8, 10, 12, 2],
+    }, {
+      name: '192.168.5',
+      data: [7, 12, 10, 8, 10, 4, 2],
+    }, {
+      name: '192.168.64',
+      data: [1, 3, 5, 7, 9, 11, 6],
+    }],
+    chart: {
+      height: 350,
+      type: 'radar',
+      dropShadow: {
+        enabled: true,
+        blur: 1,
+        left: 1,
+        top: 1
+      }
+    },
+    stroke: {
+      width: 2
+    },
+    fill: {
+      opacity: 0.1
+    },
+    markers: {
+      size: 0
+    },
+    xaxis: {
+      categories: ['No Login History', 'Drive Size No Change', 'Listen port No change', 'Established', 'RAM Usage Exceeded', 'CPU Usage Exceeded','Running Process Exceeded']
+    }
+    };
+    var DocumentRaderChart = new ApexCharts(
+      document.querySelector("#DocumentRaderChart"),
+    DocumentRaderChartOptions
+  );
+  DocumentRaderChart.render();
 };
 /* Controller
 ------------------------------------------------ */
