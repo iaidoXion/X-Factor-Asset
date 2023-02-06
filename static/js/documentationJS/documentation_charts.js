2ad8da23928afaa7d59dc99e347c28149b2bdc97
smKim <smkim@xionits.com>
2023-02-06 09:36:54 +0900

    ※수정 사항 게이지 차트 추가

--- static/js/documentationJS/documentation_charts.js
+++ static/js/documentationJS/documentation_charts.js
@@ -370,8 +370,94 @@ var handleRenderGuideApexChart = function () {
   DocumentDonutChart.render();
   // END Guide_DonutChart
 //--------------------------------------------------------------------------
-  // BEGIN Guide_CircleChart
-  var DocumentGaugeChartOptions = {
+  // BEGIN Guide_gaugeChart95
+  var DocumentGaugeChart95Options = {
+    series: [95],
+    chart: {
+      height: 350,
+      type: 'radialBar',
+      toolbar: {
+        show: true
+      }
+    },
+    plotOptions: {
+      radialBar: {
+        startAngle: -135,
+        endAngle: 225,
+        hollow: {
+          margin: 0,
+          size: '70%',
+          background: '#fff',
+          image: undefined,
+          imageOffsetX: 0,
+          imageOffsetY: 0,
+          position: 'front',
+          dropShadow: {
+            enabled: true,
+            top: 3,
+            left: 0,
+            blur: 4,
+            opacity: 0.24
+          }
+        },
+        track: {
+          background: '#fff',
+          strokeWidth: '67%',
+          margin: 0, // margin is in pixels
+          dropShadow: {
+            enabled: true,
+            top: -3,
+            left: 0,
+            blur: 4,
+            opacity: 0.35
+          }
+        },
+        dataLabels: {
+          show: true,
+          name: {
+            offsetY: -10,
+            show: true,
+            color: '#888',
+            fontSize: '17px'
+          },
+          value: {
+            formatter: function(val) {
+              return parseInt(val);
+            },
+            color: '#111',
+            fontSize: '36px',
+            show: true,
+          }
+        }
+      }
+    },
+    fill: {
+      type: 'gradient',
+      gradient: {
+        shade: 'dark',
+        type: 'horizontal',
+        shadeIntensity: 0.5,
+        gradientToColors: ['#ABE5A1'],
+        inverseColors: true,
+        opacityFrom: 1,
+        opacityTo: 1,
+        stops: [0, 100]
+      }
+    },
+    stroke: {
+      lineCap: 'round'
+    },
+    labels: ['0'],
+  };
+    var DocumentGaugeChart95 = new ApexCharts(
+      document.querySelector("#DocumentGaugeChart95"),
+    DocumentGaugeChart95Options
+  );
+  DocumentGaugeChart95.render();
+  // BEGIN Guide_gaugeChart95
+//--------------------------------------------------------------------------
+  // BEGIN Guide_gaugeChart75
+  var DocumentGaugeChart75Options = {
     series: [75],
     chart: {
       height: 350,
@@ -449,11 +535,98 @@ var handleRenderGuideApexChart = function () {
     },
     labels: ['0'],
   };
-    var DocumentGaugeChart = new ApexCharts(
-      document.querySelector("#DocumentGaugeChart"),
-    DocumentGaugeChartOptions
+    var DocumentGaugeChart75 = new ApexCharts(
+      document.querySelector("#DocumentGaugeChart75"),
+    DocumentGaugeChart75Options
   );
-  DocumentGaugeChart.render();
+  DocumentGaugeChart75.render();
+  // BEGIN Guide_gaugeChart75
+//--------------------------------------------------------------------------
+  // BEGIN Guide_gaugeChart60
+  var DocumentGaugeChart60Options = {
+    series: [60],
+    chart: {
+      height: 350,
+      type: 'radialBar',
+      toolbar: {
+        show: true
+      }
+    },
+    plotOptions: {
+      radialBar: {
+        startAngle: -135,
+        endAngle: 225,
+        hollow: {
+          margin: 0,
+          size: '70%',
+          background: '#fff',
+          image: undefined,
+          imageOffsetX: 0,
+          imageOffsetY: 0,
+          position: 'front',
+          dropShadow: {
+            enabled: true,
+            top: 3,
+            left: 0,
+            blur: 4,
+            opacity: 0.24
+          }
+        },
+        track: {
+          background: '#fff',
+          strokeWidth: '67%',
+          margin: 0, // margin is in pixels
+          dropShadow: {
+            enabled: true,
+            top: -3,
+            left: 0,
+            blur: 4,
+            opacity: 0.35
+          }
+        },
+        dataLabels: {
+          show: true,
+          name: {
+            offsetY: -10,
+            show: true,
+            color: '#888',
+            fontSize: '17px'
+          },
+          value: {
+            formatter: function(val) {
+              return parseInt(val);
+            },
+            color: '#111',
+            fontSize: '36px',
+            show: true,
+          }
+        }
+      }
+    },
+    fill: {
+      type: 'gradient',
+      gradient: {
+        shade: 'dark',
+        type: 'horizontal',
+        shadeIntensity: 0.5,
+        gradientToColors: ['#ABE5A1'],
+        inverseColors: true,
+        opacityFrom: 1,
+        opacityTo: 1,
+        stops: [0, 100]
+      }
+    },
+    stroke: {
+      lineCap: 'round'
+    },
+    labels: ['0'],
+  };
+    var DocumentGaugeChart60 = new ApexCharts(
+      document.querySelector("#DocumentGaugeChart60"),
+    DocumentGaugeChart60Options
+  );
+  DocumentGaugeChart60.render();
+  // BEGIN Guide_gaugeChart60
 //--------------------------------------------------------------------------
   // BEGIN Guide_CircleChart
   var DocumentRaderChartOptions = {
